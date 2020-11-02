0773da66ec3841cebc07014cf7310416babf658b
Toyib Ahmed <ahmedtoyib1@gmail.com>
2020-11-03 00:12:34 +0200

    add some styled

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { Balance } from "./components/Balance";
 import { IncomeExpense } from "./components/IncomeExpense";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
+import * as Styled from "./App.styled";
 
 import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <GlobalProvider>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpense />
-        <TransactionList />
-        <AddTransaction />
-      </div>
-    </GlobalProvider>
+    <Styled.Main>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpense />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
+    </Styled.Main>
   );
 }
 
---
+++ src/App.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Main = styled.div(() => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+}));
